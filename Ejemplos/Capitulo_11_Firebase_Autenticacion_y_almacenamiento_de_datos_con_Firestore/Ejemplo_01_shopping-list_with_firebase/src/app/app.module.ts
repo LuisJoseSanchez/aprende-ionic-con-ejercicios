f24c784e06ed730f91d0baca185eef2f57165ef8
Luis José Sánchez <luisjoseprofe@gmail.com>
2020-01-19 16:49:07 +0100

    AngularFire configured

--- Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_01_shopping-list_with_firebase/src/app/app.module.ts
+++ Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_01_shopping-list_with_firebase/src/app/app.module.ts
@@ -9,10 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule.enablePersistence()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_01_shopping-list_with_firebase/src/environments/environment.ts
+++ Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_01_shopping-list_with_firebase/src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA3FFrNeax94-EhrZX9u_no8GmU5BtvbKs",
+    authDomain: "shopping-list-22e6c.firebaseapp.com",
+    databaseURL: "https://shopping-list-22e6c.firebaseio.com",
+    projectId: "shopping-list-22e6c",
+    storageBucket: "shopping-list-22e6c.appspot.com",
+    messagingSenderId: "717282899513",
+    appId: "1:717282899513:web:76f60a74fd14025ed6ffe8"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
