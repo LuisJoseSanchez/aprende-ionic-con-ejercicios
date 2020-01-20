87a27bc6a76dbc1b94b023af272662e9d1bffdcd
Luis José Sánchez <luisjoseprofe@gmail.com>
2020-01-20 16:56:33 +0100

    Updates

--- Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_01_shopping-list_with_firebase/src/environments/environment.ts
+++ Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_01_shopping-list_with_firebase/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyA3FFrNeax94-EhrZX9u_no8GmU5BtvbKs",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     authDomain: "shopping-list-22e6c.firebaseapp.com",
     databaseURL: "https://shopping-list-22e6c.firebaseio.com",
     projectId: "shopping-list-22e6c",
     storageBucket: "shopping-list-22e6c.appspot.com",
-    messagingSenderId: "717282899513",
-    appId: "1:717282899513:web:76f60a74fd14025ed6ffe8"
+    messagingSenderId: "XXXXXXXXXXXXXXXXXX3",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXX"
   }
 };
