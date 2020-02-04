766763fdfc4ed8d2dcd0821db421ebd9439541e1
Luis José Sánchez <luisjoseprofe@gmail.com>
2020-02-04 11:12:34 +0100

    Updates

--- Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_02_shopping-list_with_authentication/src/app/app-routing.module.ts
+++ Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_02_shopping-list_with_authentication/src/app/app-routing.module.ts
@@ -3,22 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'list', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'list',
-    loadChildren: () => import('./pages/list/list.module').then( m => m.ListPageModule)
-  },
-  {
-    path: 'create-item',
-    loadChildren: () => import('./pages/form/form.module').then( m => m.FormPageModule)
-  },
-  {
-    path: 'edit-item/:id',
-    loadChildren: () => import('./pages/form/form.module').then( m => m.FormPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  { path: 'list',
+    loadChildren: () => import('./pages/list/list.module')
+                          .then( m => m.ListPageModule) },
+  { path: 'create-item',
+    loadChildren: () => import('./pages/form/form.module')
+                          .then( m => m.FormPageModule) },
+  { path: 'edit-item/:id',
+    loadChildren: () => import('./pages/form/form.module')
+                          .then( m => m.FormPageModule) },
+  { path: 'login',
+    loadChildren: () => import('./pages/login/login.module')
+                          .then(m=>m.LoginPageModule)
   },
 ];
 
