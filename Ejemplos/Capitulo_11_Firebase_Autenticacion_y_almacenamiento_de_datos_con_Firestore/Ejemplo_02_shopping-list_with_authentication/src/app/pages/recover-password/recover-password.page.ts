1ad7a9875c7e8f269a0d5bb8d3bc170a92db699a
Luis José Sánchez <luisjoseprofe@gmail.com>
2020-02-11 12:58:26 +0100

    Reset password page and other updates

--- Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_02_shopping-list_with_authentication/src/app/pages/recover-password/recover-password.page.ts
+++ Ejemplos/Capitulo_11_Firebase_Autenticacion_y_almacenamiento_de_datos_con_Firestore/Ejemplo_02_shopping-list_with_authentication/src/app/pages/recover-password/recover-password.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recover-password',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoverPasswordPage implements OnInit {
 
-  constructor() { }
+  email: string;
+
+  constructor(
+    private authService: AuthService,
+    private alertController: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  resetPassword() {
+    this.authService.resetPassword(this.email)
+      .then(
+        () => {
+          this.alertResetPassword();
+          this.router.navigateByUrl('/login');
+        }
+      ).catch(
+        () => this.alertError()
+      );
+  }
+
+  async alertResetPassword() {
+    const alert = await this.alertController.create({
+      header: 'Recuperación de contraseña',
+      message: 'Se le ha enviado un correo electrónico con un enlace que le permitirá recuperar la contraseña.',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
+
+  async alertError() {
+    const alert = await this.alertController.create({
+      header: 'Recuperación de contraseña',
+      message: 'No se ha podido enviar correo para reestablecer la contraseña. Inténtelo de nuevo más tarde.',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
 }
